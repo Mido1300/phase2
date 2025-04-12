06753973f20e628366e47107044e17b8ffcc67e3
Mido1300 <mohamed00112006@gmail.com>
2025-04-12 13:53:20 +0200

    Create eslint-config.mjs

---
+++ eslint-config.mjs
@@ -0,0 +1,14 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+];
